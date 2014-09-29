9374671cfe74a9378ea5fc61ef75abd2c03d5ada
olenakit <olenakit@hotmail.com>
2014-09-29 17:45:02 -0400

    exercise 2 fancy math

--- exercise2.py
+++ exercise2.py
@@ -6,18 +6,12 @@
     Assignment 1, Exercise 2, INF1340 Fall 2014
 """
 
-__author__ = 'Susan Sim'
-__email__ = "ses@drsusansim.org"
-
-__copyright__ = "2014 Susan Sim"
-__license__ = "MIT License"
-
-__status__ = "Prototype"
+__author__ = 'Natasha Gandhi, Olena Kit'
+__email__ = "natasha's email, olena.kit@mail.utoronto.ca"
 
 # imports one per line
 
-
-def checksum (upc):
+def checksum(upc):
     """
     Checks if the digits in a UPC is consistent with checksum
 
@@ -26,21 +20,52 @@ def checksum (upc):
         Boolean: True, checksum is correct
         False, otherwise
     :raises:
-        TypeError if input is not a strong
+        TypeError if input is not a string
         ValueError if string is the wrong length (with error string stating how many digits are over or under
     """
 
+    def input(upc):
+        """
+        Check if the upc is the right type or right length.
+        :param upc: a 12-digit universal product code
+        :return: type ('string') or length
+        """
+
+        if checksum(upc) == 12:
+            print("True")
+
+
+
+
+            If
+
+        return type
+
+        def len(upc):
+            return len
+
+
     # check type of input
+
     # raise TypeError if not string
 
     # check length of string
     # raise ValueError if not 12
 
     # convert string to array
-    # hint: use the list function
+
+    upc_list = list(upc)
+
+    # convert list of strings to list of integers
+
+    up = [int (s) for s in upc_list]
 
     # generate checksum using the first 11 digits provided
+
+    generate_checksum = (10-(((up[0]+up[2]+up[4]+up[6]+up[8]+up[10])*3)+(up[1]+up[3]+up[5]+up[7]+up[9]))%10)
+
     # check against the the twelfth digit
+
 
     # return True if they are equal, False otherwise
 
